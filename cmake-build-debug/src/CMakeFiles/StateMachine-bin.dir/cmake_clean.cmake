a4acec1c266103c2bfb207fbe3f98b9d22ec1486
Student User <studentuser@Students-MacBook-Pro.local>
2019-01-16 00:49:10 -0800

    Initial Base commit - project structure

---
+++ cmake-build-debug/src/CMakeFiles/StateMachine-bin.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StateMachine-bin.dir/Machine.cpp.o"
+  "CMakeFiles/StateMachine-bin.dir/States/AbstractState.cpp.o"
+  "CMakeFiles/StateMachine-bin.dir/States/NormalState.cpp.o"
+  "CMakeFiles/StateMachine-bin.dir/States/SoldOutState.cpp.o"
+  "CMakeFiles/StateMachine-bin.dir/main.cpp.o"
+  "../../bin/StateMachine.pdb"
+  "../../bin/StateMachine"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/StateMachine-bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+// main.cpp
+#include <iostream>
+
+
+int main(int argc, char** argv)
+{
+    cout<< "Build Succesful";
+    return 0;
+}
