b886663c53cd595fd044ac32d329863928e57c46
Student User <studentuser@Students-MacBook-Pro.local>
2019-01-16 00:49:10 -0800

    Initial Base commit - project structure

---
+++ cmake-build-debug/src/CMakeFiles/StateMachine-bin.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/studentuser/Desktop/SemGen-cpp/src/Machine.cpp" "/Users/studentuser/Desktop/SemGen-cpp/cmake-build-debug/src/CMakeFiles/StateMachine-bin.dir/Machine.cpp.o"
+  "/Users/studentuser/Desktop/SemGen-cpp/src/States/AbstractState.cpp" "/Users/studentuser/Desktop/SemGen-cpp/cmake-build-debug/src/CMakeFiles/StateMachine-bin.dir/States/AbstractState.cpp.o"
+  "/Users/studentuser/Desktop/SemGen-cpp/src/States/NormalState.cpp" "/Users/studentuser/Desktop/SemGen-cpp/cmake-build-debug/src/CMakeFiles/StateMachine-bin.dir/States/NormalState.cpp.o"
+  "/Users/studentuser/Desktop/SemGen-cpp/src/States/SoldOutState.cpp" "/Users/studentuser/Desktop/SemGen-cpp/cmake-build-debug/src/CMakeFiles/StateMachine-bin.dir/States/SoldOutState.cpp.o"
+  "/Users/studentuser/Desktop/SemGen-cpp/src/main.cpp" "/Users/studentuser/Desktop/SemGen-cpp/cmake-build-debug/src/CMakeFiles/StateMachine-bin.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
