e1b67041144ea50bb25fed58bb88cf59f2f8366f
0u812 <0u812@users.noreply.github.com>
2019-06-21 16:54:30 -0700

    change wstring to string in definitions files

--- semsim/definitions/OtherKBconstants.cpp
+++ semsim/definitions/OtherKBconstants.cpp
@@ -1,15 +1,12 @@
 #include "OtherKBconstants.h"
 
 namespace semsim {
-    namespace definitions {
-
-        const std::wstring OtherKBconstants::BRAUNSCHWEIG_ENZYME_DATABASE_FULLNAME = "Braunschweig Enzyme Database";
-        const std::wstring OtherKBconstants::KYOTO_ENCYCLOPEDIA_OF_GENES_AND_GENOMES_COMPOUND_KB_FULLNAME = "Kyoto Encyclopedia of Genes and Genomes - Compound";
-        const std::wstring OtherKBconstants::KYOTO_ENCYCLOPEDIA_OF_GENES_AND_GENOMES_DRUG_KB_FULLNAME = "Kyoto Encyclopedia of Genes and Genomes - Drug";
-        const std::wstring OtherKBconstants::KYOTO_ENCYCLOPEDIA_OF_GENES_AND_GENOMES_GENES_KB_FULLNAME = "Kyoto Encyclopedia of Genes and Genomes - Genes";
-        const std::wstring OtherKBconstants::KYOTO_ENCYCLOPEDIA_OF_GENES_AND_GENOMES_ORTHOLOGY_KB_FULLNAME = "Kyoto Encyclopedia of Genes and Genomes - Orthology";
-        const std::wstring OtherKBconstants::KYOTO_ENCYCLOPEDIA_OF_GENES_AND_GENOMES_PATHWAY_KB_FULLNAME = "Kyoto Encyclopedia of Genes and Genomes - Pathway";
-        const std::wstring OtherKBconstants::KYOTO_ENCYCLOPEDIA_OF_GENES_AND_GENOMES_REACTION_KB_FULLNAME = "Kyoto Encyclopedia of Genes and Genomes - Reaction";
-        const std::wstring OtherKBconstants::UNIPROT_FULLNAME = "Universal Protein Resource";
-    }
+    const std::string OtherKBconstants::BRAUNSCHWEIG_ENZYME_DATABASE_FULLNAME = "Braunschweig Enzyme Database";
+    const std::string OtherKBconstants::KYOTO_ENCYCLOPEDIA_OF_GENES_AND_GENOMES_COMPOUND_KB_FULLNAME = "Kyoto Encyclopedia of Genes and Genomes - Compound";
+    const std::string OtherKBconstants::KYOTO_ENCYCLOPEDIA_OF_GENES_AND_GENOMES_DRUG_KB_FULLNAME = "Kyoto Encyclopedia of Genes and Genomes - Drug";
+    const std::string OtherKBconstants::KYOTO_ENCYCLOPEDIA_OF_GENES_AND_GENOMES_GENES_KB_FULLNAME = "Kyoto Encyclopedia of Genes and Genomes - Genes";
+    const std::string OtherKBconstants::KYOTO_ENCYCLOPEDIA_OF_GENES_AND_GENOMES_ORTHOLOGY_KB_FULLNAME = "Kyoto Encyclopedia of Genes and Genomes - Orthology";
+    const std::string OtherKBconstants::KYOTO_ENCYCLOPEDIA_OF_GENES_AND_GENOMES_PATHWAY_KB_FULLNAME = "Kyoto Encyclopedia of Genes and Genomes - Pathway";
+    const std::string OtherKBconstants::KYOTO_ENCYCLOPEDIA_OF_GENES_AND_GENOMES_REACTION_KB_FULLNAME = "Kyoto Encyclopedia of Genes and Genomes - Reaction";
+    const std::string OtherKBconstants::UNIPROT_FULLNAME = "Universal Protein Resource";
 }
--- semsim/definitions/PropertyType.h
+++ semsim/definitions/PropertyType.h
@@ -2,79 +2,76 @@
 #include <vector>
 
 namespace semsim {
-    namespace definitions {
+    /**
+     * Enumeration of the main types of physical properties
+     * represented in biosimulation models. These correspond to the main
+     * classes of physical properties in the Ontology of Physics for Biology.
+     *
+     * - Properties of physical entities ("State properties"). Examples: The chemical
+     * concentration of a molecular species, the fluid volume of a portion of blood.
+     *
+     * - Properties of physical processes ("Rate properties"). Examples: The rate of
+     * the phosphofructokinase reaction, the rate of blood flow through an artery.
+     *
+     * * - Properties of physical forces ("Force properties"). Examples: Electrical
+     * potential across a cell membrane, fluid pressure in an artery.
+     *
+     * - Properties of physical dependencies ("Constitutive properties"). Examples:
+     * A first order reaction rate constant used to determine a reaction rate,
+     *  the fluid resistance term in an instance of Ohm's Law for fluids.
+     */
+    class PropertyType {
+      public:
+        static PropertyType PropertyOfPhysicalEntity;
+        static PropertyType PropertyOfPhysicalProcess;
+        static PropertyType PropertyOfPhysicalForce;
+        static PropertyType Unknown;
+
+      private:
+        // static std::vector<PropertyType> valueList;
+
+        class StaticConstructor {
+          public:
+            StaticConstructor();
+        };
+
+        static StaticConstructor staticConstructor;
+
+      public:
+        enum class InnerEnum {
+            PropertyOfPhysicalEntity,
+            PropertyOfPhysicalProcess,
+            PropertyOfPhysicalForce,
+            Unknown
+        };
+
+        const InnerEnum innerEnumValue;
+      private:
+        const std::string nameValue;
+        const int ordinalValue;
+        static int nextOrdinal;
+
+        // String representation of enum
+      private:
+        const std::string _name;
+
+        PropertyType(const std::string& name, InnerEnum innerEnum, const std::string& name);
 
         /**
-         * Enumeration of the main types of physical properties
-         * represented in biosimulation models. These correspond to the main
-         * classes of physical properties in the Ontology of Physics for Biology.
-         *
-         * - Properties of physical entities ("State properties"). Examples: The chemical
-         * concentration of a molecular species, the fluid volume of a portion of blood.
-         *
-         * - Properties of physical processes ("Rate properties"). Examples: The rate of
-         * the phosphofructokinase reaction, the rate of blood flow through an artery.
-         *
-         * * - Properties of physical forces ("Force properties"). Examples: Electrical
-         * potential across a cell membrane, fluid pressure in an artery.
-         *
-         * - Properties of physical dependencies ("Constitutive properties"). Examples:
-         * A first order reaction rate constant used to determine a reaction rate,
-         *  the fluid resistance term in an instance of Ohm's Law for fluids.
+         * Returns the name of the property type as a string.
          */
-        class PropertyType {
-          public:
-            static PropertyType PropertyOfPhysicalEntity;
-            static PropertyType PropertyOfPhysicalProcess;
-            static PropertyType PropertyOfPhysicalForce;
-            static PropertyType Unknown;
+      public:
+        virtual std::string ToString();
 
-          private:
-            // static std::vector<PropertyType> valueList;
+      public:
+        bool operator == (const PropertyType& other);
 
-            class StaticConstructor {
-              public:
-                StaticConstructor();
-            };
+        bool operator != (const PropertyType& other);
 
-            static StaticConstructor staticConstructor;
+        static std::vector<PropertyType> values();
 
-          public:
-            enum class InnerEnum {
-                PropertyOfPhysicalEntity,
-                PropertyOfPhysicalProcess,
-                PropertyOfPhysicalForce,
-                Unknown
-            };
+        int ordinal();
 
-            const InnerEnum innerEnumValue;
-          private:
-            const std::wstring nameValue;
-            const int ordinalValue;
-            static int nextOrdinal;
-
-            // String representation of enum
-          private:
-            const std::wstring _name;
-
-            PropertyType(const std::wstring& name, InnerEnum innerEnum, const std::wstring& name);
-
-            /**
-             * Returns the name of the property type as a string.
-             */
-          public:
-            virtual std::wstring ToString();
-
-          public:
-            bool operator == (const PropertyType& other);
-
-            bool operator != (const PropertyType& other);
-
-            static std::vector<PropertyType> values();
-
-            int ordinal();
-
-            static PropertyType valueOf(const std::wstring& name);
-        };
-    }
+        static PropertyType valueOf(const std::string& name);
+    };
 }
