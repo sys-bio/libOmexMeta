ef4586aa9aa3c9195ed09a3cc1f2c19609936468
0u812 <0u812@users.noreply.github.com>
2019-06-25 14:13:33 -0700

    disable utilities

--- semsim/utilities/ErrorEntry.cpp
+++ semsim/utilities/ErrorEntry.cpp
@@ -1,16 +1,16 @@
-#include "ErrorEntry.h"
-
-namespace semsim {
-    namespace utilities {
-
-        ErrorEntry::ErrorEntry(const std::wstring& message, std::optional<bool>& fatal) {
-            errmsg = message;
-            isfatal = fatal;
-        }
-
-        int ErrorEntry::compareTo(ErrorEntry* arg0) {
-            // TODO Auto-generated method stub
-            return 0;
-        }
-    }
-}
+// #include "ErrorEntry.h"
+//
+// namespace semsim {
+//     namespace utilities {
+//
+//         ErrorEntry::ErrorEntry(const std::wstring& message, std::optional<bool>& fatal) {
+//             errmsg = message;
+//             isfatal = fatal;
+//         }
+//
+//         int ErrorEntry::compareTo(ErrorEntry* arg0) {
+//             // TODO Auto-generated method stub
+//             return 0;
+//         }
+//     }
+// }
