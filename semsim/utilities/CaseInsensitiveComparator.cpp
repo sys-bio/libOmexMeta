57eadd8c6afd528475a069d1b28b28c79909d1a3
0u812 <0u812@users.noreply.github.com>
2019-06-25 14:13:33 -0700

    disable utilities

--- semsim/utilities/CaseInsensitiveComparator.cpp
+++ semsim/utilities/CaseInsensitiveComparator.cpp
@@ -1,10 +1,10 @@
-#include "CaseInsensitiveComparator.h"
-
-namespace semsim {
-    namespace utilities {
-
-        int CaseInsensitiveComparator::compare(const std::wstring& strA, const std::wstring& strB) {
-            return strA.compareToIgnoreCase(strB);
-        }
-    }
-}
+// #include "CaseInsensitiveComparator.h"
+//
+// namespace semsim {
+//     namespace utilities {
+//
+//         int CaseInsensitiveComparator::compare(const std::wstring& strA, const std::wstring& strB) {
+//             return strA.compareToIgnoreCase(strB);
+//         }
+//     }
+// }
--- semsim/utilities/SemSimComponentComparator.cpp
+++ semsim/utilities/SemSimComponentComparator.cpp
@@ -1,11 +1,11 @@
-#include "SemSimComponentComparator.h"
-
-namespace semsim {
-    namespace utilities {
-        using SemSimObject = semsim::SemSimObject;
-
-        int SemSimComponentComparator::compare(SemSimObject* A, SemSimObject* B) {
-            return A->getName().compareToIgnoreCase(B->getName());
-        }
-    }
-}
+// #include "SemSimComponentComparator.h"
+//
+// namespace semsim {
+//     namespace utilities {
+//         using SemSimObject = semsim::SemSimObject;
+//
+//         int SemSimComponentComparator::compare(SemSimObject* A, SemSimObject* B) {
+//             return A->getName().compareToIgnoreCase(B->getName());
+//         }
+//     }
+// }
