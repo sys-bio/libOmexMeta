9a988d7ca799f93dbe7c1325bedcc3af28fbd04d
0u812 <0u812@users.noreply.github.com>
2019-07-02 13:05:48 -0700

    write out sbml

--- examples/create_sbml_model.cpp
+++ examples/create_sbml_model.cpp
@@ -6,25 +6,27 @@
 
 int main() {
   // create SBML model
-  LIBSBML_CPP_NAMESPACE_QUALIFIER SBMLDocument* sbmlDoc =
+  LIBSBML_CPP_NAMESPACE_QUALIFIER SBMLDocument* d =
     new LIBSBML_CPP_NAMESPACE_QUALIFIER SBMLDocument(3, 2);
-  LIBSBML_CPP_NAMESPACE_QUALIFIER Model* model = sbmlDoc->createModel();
-  model->setId("MyModel");
+  LIBSBML_CPP_NAMESPACE_QUALIFIER Model* model = d->createModel();
+  model->setId("beta_cell_model");
 
   LIBSBML_CPP_NAMESPACE_QUALIFIER Compartment* comp = model->createCompartment();
-  comp->setId("MyCompartment");
+  comp->setId("cytosol");
   comp->setSize(1);
   comp->setConstant(true);
 
   // create the species for glucose
   LIBSBML_CPP_NAMESPACE_QUALIFIER Species* s = model->createSpecies();
-  s->setCompartment("MyCompartment");
+  s->setCompartment("cytosol");
   s->setId("glucose");
   s->setInitialAmount(0);
 
   // create import reaction for glucose
   LIBSBML_CPP_NAMESPACE_QUALIFIER Reaction* reaction = model->createReaction();
   reaction->setId("glucose_import");
+  LIBSBML_CPP_NAMESPACE_QUALIFIER SpeciesReference* sr = reaction->createProduct();
+  sr->setSpecies("glucose");
   LIBSBML_CPP_NAMESPACE_QUALIFIER KineticLaw* k = reaction->createKineticLaw();
   LIBSBML_CPP_NAMESPACE_QUALIFIER Parameter* p = k->createParameter();
   p->setId("glucose_import_rate");
@@ -35,6 +37,10 @@ int main() {
   //   new LIBSBML_CPP_NAMESPACE_QUALIFIER ASTNode(LIBSBML_CPP_NAMESPACE_QUALIFIER AST_NAME);
   // a->setName("glucose_import_rate");
   // k->setMath(a);
+
+  LIBSBML_CPP_NAMESPACE_QUALIFIER SBMLWriter sbml_writer;
+
+  std::cerr << "SBML output:\n" << sbml_writer.writeSBMLToString(d) << "\n";
 
   return 0;
 }
