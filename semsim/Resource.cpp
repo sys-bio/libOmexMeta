7466425cd461dbbd667b4d612e279cb54b583aa7
0u812 <0u812@users.noreply.github.com>
2019-07-14 19:50:57 -0700

    add ontology prefixes to humanized terms

--- semsim/Resource.cpp
+++ semsim/Resource.cpp
@@ -14,15 +14,15 @@ namespace semsim {
     if (element_)
       return "#"+element_->getAnnotation().getMetaId();
     else if (uri_ == "https://identifiers.org/GO:0005829")
-      return "cytoplasm";
+      return "GO:cytoplasm";
     else if (uri_ == "https://identifiers.org/CL:0000169")
-      return "type B pancreatic cell";
+      return "CL:type B pancreatic cell";
     else if (uri_ == "http://identifiers.org/CHEBI:17234")
-      return "glucose";
+      return "CHEBI:glucose";
     else if (uri_ == "http://identifiers.org/opb/OPB_00425")
-      return "chemical concentration";
+      return "OPB:chemical concentration";
     else if (uri_ == "http://identifiers.org/opb/OPB_00523")
-      return "spatial volume";
+      return "OPB:spatial volume";
     else
       throw std::runtime_error("Resource not recognized "+uri_.toString());
   }
