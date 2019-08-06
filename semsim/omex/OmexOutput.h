62e1e294b2ff2b6ecbf0a3fa184feb97e7c51a9b
0u812 <0u812@users.noreply.github.com>
2019-08-06 13:30:44 -0700

    fix docs

--- semsim/omex/OmexOutput.h
+++ semsim/omex/OmexOutput.h
@@ -9,10 +9,10 @@ namespace semsim {
     /**
      * Write a @ref Model and associated annotation information to a
      * Combine archive.
-     * @param model           The libSemSim @ref Model to write out.
-     * @param archive_path    The path for the Combine archive.
-     * @param sbml_entry_path The path to the SBML file within the archive.
-     * @param rdf_entry_path  The path to the annotation RDF metadata within the archive.
+     * @param model            The libSemSim @ref Model to write out.
+     * @param archive_path     The path for the Combine archive.
+     * @param model_entry_path The path to the model file (SBML or CellML) within the archive.
+     * @param rdf_entry_path   The path to the annotation RDF metadata within the archive.
      */
     void exportToOmex(const Model& model,
       const std::string& archive_path,
