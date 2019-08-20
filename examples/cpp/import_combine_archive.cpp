388c5ebf8264225d809a7ac7de3e5983ab52119d
0u812 <0u812@users.noreply.github.com>
2019-08-20 09:46:47 -0700

    use cmd line args for import example

--- examples/cpp/import_combine_archive.cpp
+++ examples/cpp/import_combine_archive.cpp
@@ -4,8 +4,11 @@
 
 using namespace semsim;
 
-int main() {
-  SBMLModel model = importSBMLOmex("/tmp/combine.omex", "model.xml", "annotations.xml");
+int main(int argc, char *argv[]) {
+  if (argc < 2)
+    throw std::runtime_error("Usage: import_combine_archive filename path-to-sbml path-to-annotations");
+
+  SBMLModel model = importSBMLOmex(std::string(argv[1]), std::string(argv[2]), std::string(argv[3]));
 
   std::cerr << "**********************************************************\n";
   std::cerr << "RDF annotations\n";
