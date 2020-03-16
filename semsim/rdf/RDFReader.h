48eb629e5af831a7f92c90208b4c8404f268a079
Ciaran Welsh <cwelsh2@uw.edu>
2020-03-16 19:38:02 +0000

    put RDFReader content into a class instead of a bunch of top level functions

--- semsim/rdf/RDFReader.h
+++ semsim/rdf/RDFReader.h
@@ -6,42 +6,47 @@
 # include "semsim/sbml/SBMLModel.h"
 
 namespace semsim {
-    static std::string RaptorURIToString(raptor_uri *u);
+
+    class RDFReader {
+    public:
+        static std::string RaptorURIToString(raptor_uri *u);
 
 
-    static URI RaptorTermToURI(raptor_term *t);
+        static URI RaptorTermToURI(raptor_term *t);
 
-    static std::string RaptorTermToRepr(raptor_term *t);
+        static std::string RaptorTermToRepr(raptor_term *t);
 
-    static bool isMetaId(const std::string &uri);
+        static bool isMetaId(const std::string &uri);
 
-    static bool isMetaId(raptor_term *t);
+        static bool isMetaId(raptor_term *t);
 
-    static std::string extractMetaId(const std::string &uri);
+        static std::string extractMetaId(const std::string &uri);
 
-    static std::string extractMetaId(raptor_term *t);
+        static std::string extractMetaId(raptor_term *t);
 
-    static void addTermToEntity(EntityBase &entity, const Relation &relation, const Resource &resource);
+        static void addTermToEntity(EntityBase &entity, const Relation &relation, const Resource &resource);
 
-    static void
-    addTermToCompositeAnnotation(CompositeAnnotation &annotation, const Relation &relation, const Resource &resource);
+        static void
+        addTermToCompositeAnnotation(CompositeAnnotation &annotation, const Relation &relation,
+                                     const Resource &resource);
 
-    static void
-    addTermToSingularAnnotation(SingularAnnotation &annotation, const Relation &relation, const Resource &resource);
+        static void
+        addTermToSingularAnnotation(SingularAnnotation &annotation, const Relation &relation, const Resource &resource);
 
-    static void addTermToAnnotation(AnnotationBase &annotation, const Relation &relation, const Resource &resource) ;
+        static void addTermToAnnotation(AnnotationBase &annotation, const Relation &relation, const Resource &resource);
 
-    static void process_triple(void *user_data, raptor_statement *triple) ;
+        static void process_triple(void *user_data, raptor_statement *triple);
 
-    /**
-     * Applies RDF metadata annotations to a libSemSim model.
-     * @param model      The libSemSim model.
-     * @param rdf        The RDF annotations to apply.
-     * @param rdf_format The format of the RDF annotations. Choices include "rdfxml", "ntriples", "turtle", "trig", "rss-tag-soup", "grddl", "rdfa", "json", and "nquads".
-     */
-    void applyRDFAnnotationsToModel(
-            Model &model,
-            const std::string &rdf,
-            const std::string &rdf_format = "rdfxml");
+        /**
+         * Applies RDF metadata annotations to a libSemSim model.
+         * @param model      The libSemSim model.
+         * @param rdf        The RDF annotations to apply.
+         * @param rdf_format The format of the RDF annotations. Choices include "rdfxml", "ntriples", "turtle", "trig", "rss-tag-soup", "grddl", "rdfa", "json", and "nquads".
+         */
+        void applyRDFAnnotationsToModel(
+                Model &model,
+                const std::string &rdf,
+                const std::string &rdf_format = "rdfxml");
+    };
 }
 # endif
