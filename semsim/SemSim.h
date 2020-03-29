eb0f20ff7352b3c704a55f730f1560cecfb92ebb
Ciaran Welsh <cwelsh2@uw.edu>
2020-03-29 10:46:54 +0100

    Broken. Revert to c3ead55260654c3637ca252aad108cf6b1ecf439

--- semsim/SemSim.h
+++ semsim/SemSim.h
@@ -2,7 +2,7 @@
 # define SEMSIM_H_
 
 // important classes (includes may be redundant - that's okay)
-# include "semsim/Preproc.h"
+//# include "semsim/Preproc.h"
 # include "semsim/Relation.h"
 # include "semsim/Resource.h"
 # include "semsim/EntityBase.h"
