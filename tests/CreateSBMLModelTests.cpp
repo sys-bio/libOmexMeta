0e8e6084540209b9ee248e3230be006d266ba97f
Ciaran Welsh <cwelsh2@uw.edu>
2020-03-30 12:22:07 +0100

    add Factory abstrcation around generation of SBML models for testing purposes

--- tests/CreateSBMLModelTests.cpp
+++ tests/CreateSBMLModelTests.cpp
@@ -2,105 +2,35 @@
 // Created by Ciaran on 3/28/2020.
 //
 
-# include "gtest/gtest.h"
-# include <semsim/SemSim.h>
-# include "sbml/SBMLTypes.h"
-# include <iostream>
+#include "gtest/gtest.h"
+#include <semsim/SemSim.h>
+#include "sbml/SBMLTypes.h"
+#include <iostream>
 
-using namespace semsim;
-using namespace libsbml;
+#include "SBMLFactory.h"
+
+
 
 class SBMLImporterTests : public ::testing::Test {
 protected:
     SBMLDocument *doc;
-    Model *m;
-    std::string xml;
 
-    void SetUp() override {
-
-        doc = new SBMLDocument(3, 2);
-        m = doc->createModel("beta_cell_model");
-
-        // create a compartment to represent the cytosol
-        Compartment *comp = m->createCompartment();
-        comp->setId("cytosol");
-        comp->setMetaId("cytosol");
-        comp->setSize(1);
-        comp->setConstant(true);
-
-        // add a CV term for the cytosol
-        auto *cv_cytosol = new CVTerm();
-        cv_cytosol->setQualifierType(BIOLOGICAL_QUALIFIER);
-        cv_cytosol->setBiologicalQualifierType(BQB_IS);
-        cv_cytosol->addResource(GO::get(5829).getURI().encode());
-        comp->addCVTerm(cv_cytosol);
-
-        UnitDefinition *unitdef = m->createUnitDefinition();
-        unitdef->setId("molar");
-
-        Unit *unit = unitdef->createUnit();
-        unit->setKind(UNIT_KIND_MOLE);
-        unit->setExponent(1);
-        unit->setMultiplier(1);
-        unit->setScale(1);
-
-        unit = unitdef->createUnit();
-        unit->setKind(UNIT_KIND_LITRE);
-        unit->setExponent(-1);
-        unit->setMultiplier(1);
-        unit->setScale(1);
-
-        // create the species for glucose
-        Species *s = m->createSpecies();
-        s->setCompartment("cytosol");
-        s->setId("glucose");
-        s->setMetaId("glucose");
-        s->setInitialConcentration(0);
-        s->setUnits("molar");
-        s->setHasOnlySubstanceUnits(false);
-        s->setConstant(false);
-        s->setBoundaryCondition(false);
-
-        // add a CV term to glucose
-        auto *cv_glucose = new CVTerm();
-        cv_glucose->setQualifierType(BIOLOGICAL_QUALIFIER);
-        cv_glucose->setBiologicalQualifierType(BQB_IS);
-        cv_glucose->addResource(CHEBI::get(17234).getURI().encode());
-        s->addCVTerm(cv_glucose);
-
-        // create import reaction for glucose
-        Reaction *reaction = m->createReaction();
-        reaction->setId("glucose_import");
-        reaction->setReversible(false);
-
-        SpeciesReference *sr = reaction->createProduct();
-        sr->setSpecies("glucose");
-        sr->setConstant(false);
-        KineticLaw *k = reaction->createKineticLaw();
-        Parameter *p = k->createParameter();
-        p->setId("glucose_import_rate");
-        p->setValue(1);
-        k->setMath(SBML_parseL3FormulaWithModel("glucose_import_rate", m));
-        SBMLWriter sbml_writer;
-        xml = sbml_writer.writeSBMLToString(doc);
-    }
-
-//    void TearDown() override {
-//        delete doc;
-//        delete m;
-//    }
+    SBMLImporterTests()  {
+        doc = semsim_test::SBMLFactory::getModel(semsim_test::SBML_ANNOTATED);
+    };
 };
 
-
+/*
+ * importing the model into libSemSim will automatically
+ * add metaids for any annotatable SBML elements that lack them
+ *
+ */
 TEST_F(SBMLImporterTests, test1) {
-    // create SBML model
-//     importing the model into libSemSim will automatically
-//     add metaids for any annotatable SBML elements that lack them
-    SBMLImporter importer(doc);
-    SBMLModel &model = importer.getSBMLModel();
+    semsim::SBMLImporter importer(doc);
+    semsim::SBMLModel &model = importer.getSBMLModel();
     std::string rdf = model.getRDF("./mymodel.xml", "turtle");
-    const char* actual = rdf.c_str();
-    const char* expected = "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n"
+    const char *actual = rdf.c_str();
+    const char *expected = "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n"
                            "@prefix bqb: <http://biomodels.net/biology-qualifiers/> .\n"
                            "@prefix semsim: <http://www.bhi.washington.edu/SemSim#> .\n"
                            "\n"
@@ -123,7 +53,7 @@ TEST_F(SBMLImporterTests, test1) {
                            "    semsim:hasSinkParticipant <#sink0> .\n\n";
     ASSERT_STREQ(expected, actual);
 }
-//
+
 //    // this isn't bad - the RDF already contains a composite annotation
 //    // telling is that the species "glucose" is inside the "cytosol"
 //    // we currently have "cytosolic glucose"
---
+++ tests/SBMLFactory.h
@@ -0,0 +1,194 @@
+//
+// Created by Ciaran on 3/29/2020.
+//
+
+#ifndef LIBSEMGEN_SBMLFACTORY_H
+#define LIBSEMGEN_SBMLFACTORY_H
+
+#include <utility>
+
+#include "sbml/SBMLTypes.h"
+#include "semsim/SemSim.h"
+
+using namespace libsbml;
+
+namespace semsim_test {
+
+    enum ModelType {
+        SBML_ANNOTATED,
+        SBML_NOT_ANNOTATED,
+    };
+
+/*
+ * Abstract class implemented by all SBML models
+ */
+    class SBMLModel {
+    public:
+        SBMLModel() = default;
+
+        virtual SBMLDocument *buildModel() = 0;
+
+    };
+
+    class SBMLAnnotated : SBMLModel {
+    public:
+        SBMLAnnotated() = default;
+
+        SBMLDocument *buildModel() override {
+            SBMLDocument *doc;
+            Model *m;
+            std::string xml;
+
+            doc = new SBMLDocument(3, 2);
+            m = doc->createModel("beta_cell_model");
+
+            // create a compartment to represent the cytosol
+            Compartment *comp = m->createCompartment();
+            comp->setId("cytosol");
+            comp->setMetaId("cytosol");
+            comp->setSize(1);
+            comp->setConstant(true);
+
+            // add a CV term for the cytosol
+            auto *cv_cytosol = new CVTerm();
+            cv_cytosol->setQualifierType(BIOLOGICAL_QUALIFIER);
+            cv_cytosol->setBiologicalQualifierType(BQB_IS);
+            cv_cytosol->addResource(semsim::GO::get(5829).getURI().encode());
+            comp->addCVTerm(cv_cytosol);
+
+            UnitDefinition *unitdef = m->createUnitDefinition();
+            unitdef->setId("molar");
+
+            Unit *unit = unitdef->createUnit();
+            unit->setKind(UNIT_KIND_MOLE);
+            unit->setExponent(1);
+            unit->setMultiplier(1);
+            unit->setScale(1);
+
+            unit = unitdef->createUnit();
+            unit->setKind(UNIT_KIND_LITRE);
+            unit->setExponent(-1);
+            unit->setMultiplier(1);
+            unit->setScale(1);
+
+            // create the species for glucose
+            Species *s = m->createSpecies();
+            s->setCompartment("cytosol");
+            s->setId("glucose");
+            s->setMetaId("glucose");
+            s->setInitialConcentration(0);
+            s->setUnits("molar");
+            s->setHasOnlySubstanceUnits(false);
+            s->setConstant(false);
+            s->setBoundaryCondition(false);
+
+            // add a CV term to glucose
+            auto *cv_glucose = new CVTerm();
+            cv_glucose->setQualifierType(BIOLOGICAL_QUALIFIER);
+            cv_glucose->setBiologicalQualifierType(BQB_IS);
+            cv_glucose->addResource(semsim::CHEBI::get(17234).getURI().encode());
+            s->addCVTerm(cv_glucose);
+
+            // create import reaction for glucose
+            Reaction *reaction = m->createReaction();
+            reaction->setId("glucose_import");
+            reaction->setReversible(false);
+
+            SpeciesReference *sr = reaction->createProduct();
+            sr->setSpecies("glucose");
+            sr->setConstant(false);
+            KineticLaw *k = reaction->createKineticLaw();
+            Parameter *p = k->createParameter();
+            p->setId("glucose_import_rate");
+            p->setValue(1);
+            k->setMath(SBML_parseL3FormulaWithModel("glucose_import_rate", m));
+            return doc;
+        }
+    };
+
+    class SBMLNotAnnotated : SBMLModel {
+    public:
+        SBMLNotAnnotated() = default;
+
+        SBMLDocument *buildModel() override {
+            SBMLDocument *doc;
+            Model *m;
+            std::string xml;
+
+            doc = new SBMLDocument(3, 2);
+            m = doc->createModel("beta_cell_model");
+
+            // create a compartment to represent the cytosol
+            Compartment *comp = m->createCompartment();
+            comp->setId("cytosol");
+            comp->setMetaId("cytosol");
+            comp->setSize(1);
+            comp->setConstant(true);
+
+            UnitDefinition *unitdef = m->createUnitDefinition();
+            unitdef->setId("molar");
+
+            Unit *unit = unitdef->createUnit();
+            unit->setKind(UNIT_KIND_MOLE);
+            unit->setExponent(1);
+            unit->setMultiplier(1);
+            unit->setScale(1);
+
+            unit = unitdef->createUnit();
+            unit->setKind(UNIT_KIND_LITRE);
+            unit->setExponent(-1);
+            unit->setMultiplier(1);
+            unit->setScale(1);
+
+            // create the species for glucose
+            Species *s = m->createSpecies();
+            s->setCompartment("cytosol");
+            s->setId("glucose");
+            s->setMetaId("glucose");
+            s->setInitialConcentration(0);
+            s->setUnits("molar");
+            s->setHasOnlySubstanceUnits(false);
+            s->setConstant(false);
+            s->setBoundaryCondition(false);
+
+            // create import reaction for glucose
+            Reaction *reaction = m->createReaction();
+            reaction->setId("glucose_import");
+            reaction->setReversible(false);
+
+            SpeciesReference *sr = reaction->createProduct();
+            sr->setSpecies("glucose");
+            sr->setConstant(false);
+            KineticLaw *k = reaction->createKineticLaw();
+            Parameter *p = k->createParameter();
+            p->setId("glucose_import_rate");
+            p->setValue(1);
+            k->setMath(SBML_parseL3FormulaWithModel("glucose_import_rate", m));
+            return doc;
+        }
+    };
+
+
+    class SBMLFactory {
+    private:
+        SBMLWriter writer;
+    public:
+
+        static SBMLDocument *getModel(ModelType modelType) {
+            if (modelType == SBML_ANNOTATED)
+                return SBMLAnnotated().buildModel();
+            else if (modelType == SBML_NOT_ANNOTATED)
+                return SBMLNotAnnotated().buildModel();
+            else {
+                throw std::invalid_argument("ModelType is not a valid argument.");
+            }
+        }
+
+        std::string getModelStr(ModelType modelType) {
+            SBMLDocument *doc = getModel(modelType);
+            return writer.writeSBMLToString(doc);
+        }
+    };
+}
+
+#endif //LIBSEMGEN_SBMLFACTORY_H
