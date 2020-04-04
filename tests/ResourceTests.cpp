52923196ec7ca741409eec6bb5dd9429d5c9cb38
Ciaran Welsh <cwelsh2@uw.edu>
2020-04-04 16:55:28 +0100

    built a resorce class with a nicer user interface. Tests work.

--- tests/ResourceTests.cpp
+++ tests/ResourceTests.cpp
@@ -21,16 +21,16 @@ public:
 
 TEST_F(ResourceTests, TestGetResourceNamespace) {
     semsim::Resource resource(ns, id);
-    const std::string& expected = "genecards";
-    const std::string& ns2 = resource.getResourceNamespace();
+    const std::string &expected = "genecards";
+    const std::string &ns2 = resource.getResourceNamespace();
     ASSERT_STREQ(expected.c_str(), ns2.c_str());
 }
 
 
 TEST_F(ResourceTests, TestGetUri) {
     semsim::Resource resource(ns, id);
-    const std::string& expected = "ABL1";
-    const std::string& actual = resource.getIdentifier();
+    const std::string &expected = "ABL1";
+    const std::string &actual = resource.getIdentifier();
     ASSERT_STREQ(expected.c_str(), actual.c_str());
 }
 
@@ -52,6 +52,38 @@ TEST_F(ResourceTests, TestStreamOperator) {
     os << resource1;
     std::string expected = "Resource(https://identifiers.org/genecards/ABL1)";
     ASSERT_STREQ(expected.c_str(), os.str().c_str());
+}
+
+
+TEST_F(ResourceTests, TestStringSpliter1) {
+    std::vector<std::string> expected = {"cheese"};
+    std::vector<std::string> actual = semsim::Resource::splitStringBy("cheese", ':');
+    ASSERT_EQ(expected, actual);
+}
+
+TEST_F(ResourceTests, TestStringSpliter2) {
+    std::vector<std::string> expected = {"cheese", "biscuits"};
+    std::vector<std::string> actual = semsim::Resource::splitStringBy("cheese:biscuits", ':');
+    ASSERT_EQ(expected, actual);
+}
+
+
+TEST_F(ResourceTests, TestFromString1) {
+    semsim::Resource resource1("genecards/ABL1");
+    const std::string& actual = resource1.getResourceNamespace();
+    std::string expected = "genecards";
+    ASSERT_STREQ(expected.c_str(), actual.c_str());
+}
+
+TEST_F(ResourceTests, TestFromString2) {
+    semsim::Resource resource1("genecards:ABL1");
+    const std::string& actual = resource1.getResourceNamespace();
+    std::string expected = "genecards";
+    ASSERT_STREQ(expected.c_str(), actual.c_str());
+}
+
+TEST_F(ResourceTests, TestFromString3) {
+    ASSERT_THROW(semsim::Resource resource1("genecardsABL1"), std::logic_error);
 }
 
 
