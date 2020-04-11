feabbd90bf970043d8a1df47c0159d135f307a5c
Ciaran Welsh <cwelsh2@uw.edu>
2020-04-11 19:44:24 +0100

    Added 7 test strings which correspond to the 7 examples in the omex metadata spec. Wrote 1 read and write test for each of the 7 example for each of the serialization language

---
+++ semsim/RedlandWrapper.h
@@ -0,0 +1,14 @@
+//
+// Created by Ciaran on 4/11/2020.
+//
+
+#ifndef LIBSEMGEN_REDLANDWRAPPER_H
+#define LIBSEMGEN_REDLANDWRAPPER_H
+
+
+class RedlandWrapper {
+
+};
+
+
+#endif //LIBSEMGEN_REDLANDWRAPPER_H
