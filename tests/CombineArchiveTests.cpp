4505b72dc44b302840dbbab508073d9df3df9de3
Ciaran Welsh <cwelsh2@uw.edu>
2020-04-12 13:43:20 +0100

    found a build bug with libcombine

--- tests/CombineArchiveTests.cpp
+++ tests/CombineArchiveTests.cpp
@@ -4,7 +4,8 @@
 
 #include <string>
 #include "gtest/gtest.h"
-#include "semsim/CombineArchive.h"
+#include "semsim/SemsimCombineArchive.h"
+#include "semsim/CurlGet.h"
 
 class RDFTestsCombine : public ::testing::Test {
 public:
@@ -14,12 +15,58 @@ public:
     std::string gold_standard_url4 = "https://auckland.figshare.com/ndownloader/files/15425546";
     std::string gold_standard_url5 = "https://auckland.figshare.com/ndownloader/files/17432366";
 
-    RDFTestsCombine() {
+    std::string gold_standard_filename1 = "goldstandard1.omex";
+    std::string gold_standard_filename2 = "goldstandard2.omex";
+    std::string gold_standard_filename3 = "goldstandard3.omex";
+    std::string gold_standard_filename4 = "goldstandard4.omex";
+    std::string gold_standard_filename5 = "goldstandard5.omex";
 
+    RDFTestsCombine() {
+        semsim::CurlGet::download(gold_standard_url1, gold_standard_filename1);
+    }
+
+    static int removeFile(const std::string &filename) {
+        if (std::remove(filename.c_str()) != 0)
+            perror("File deletion failed");
+        else
+            std::cout << "File deleted successfully";
+        return 0;
+    }
+
+    void TearDown() override {
+//        removeFile(gold_standard_filename1);
     }
 };
 
-TEST_F(RDFTestsCombine, tesnakjsbt) {
-    CombineArchive combineArchive(gold_standard_url1);
+TEST_F(RDFTestsCombine, TestThatUrlIsNotFile) {
+    semsim::SemsimCombineArchive combineArchive(gold_standard_url1);
+    std::string o = "/mnt/d/libsemsim/tests/gold_standard1.omex";
+    combineArchive.fromUrl(o);
+
+
+//    ASSERT_FALSE(combineArchive.isfile());
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
