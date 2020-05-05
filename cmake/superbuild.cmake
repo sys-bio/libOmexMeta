7cfd3ea0d5d5b47365c4449a6f06a496fc84a25c
Ciaran Welsh <cwelsh2@uw.edu>
2020-05-05 14:38:31 +0100

    made a mes with the build system

--- cmake/superbuild.cmake
+++ cmake/superbuild.cmake
@@ -24,19 +24,6 @@ ExternalProject_Add(libsbml-dependencies
         )
 
 
-# find zlib to give to zipper which depends on it
-find_library(ZLIB_STATIC_LIBRARY
-        NAMES libz.a z.a zlib.a libzlib.a zlib.dll libzlib.dll
-        PATHS ${ZLIB_INSTALL_PREFIX}/lib
-        REQUIRED
-        )
-
-find_path(ZLIB_INCLUDE_DIR
-        NAMES zlib.h
-        PATHS ${ZLIB_INSTALL_PREFIX}/include
-        REQUIRED
-        )
-
 # build zipper
 ExternalProject_Add(zipper
         SOURCE_DIR ${ZIPPER_SOURCE_DIR}
@@ -48,36 +35,6 @@ ExternalProject_Add(zipper
         -DLIBSBML_DEPS_INSTALL_PREFIX=${LIBSBML_DEPS_INSTALL_PREFIX}
         -DZLIB_LIBRARY=${ZLIB_STATIC_LIBRARY}
         -DZLIB_INCLUDE_DIR=${ZLIB_INCLUDE_DIR}
-        )
-
-# find zipper lib and include dirs
-find_library(ZIPPER_STATIC_LIBRARY
-        NAMES libZipper-static.a libZipper-static
-        PATHS ${ZIPPER_INSTALL_PREFIX}/lib
-        )
-find_path(ZIPPER_INCLUDE_DIR
-        NAMES zipper/zipper.h
-        PATHS ${ZIPPER_INSTALL_PREFIX}/include
-        )
-
-# find bz library and include dirs
-find_library(LIBBZ_STATIC_LIBRARY
-        NAMES libbz2.a
-        PATHS ${LIBSBML_DEPS_LIB_DIR}
-        )
-find_path(LIBBZ_INCLUDE_DIR
-        NAMES bzip2/bzlib.h
-        PATHS ${LIBSBML_DEPS_INCLUDE_DIR}
-        )
-
-# get libxml2 libraries
-find_library(LIBXML2_STATIC_LIBRARY
-        NAMES libxml2.a
-        )
-
-find_path(LIBXML2_INCLUDE_DIR
-        NAMES libxml/parser.h
-        PATHS /usr/local/include/libxml2
         )
 
 # build libsbml
@@ -95,18 +52,6 @@ ExternalProject_Add(libsbml
         -DLIBSBML_SKIP_SHARED_LIBRARY=OFF
         )
 
-# find the libsbml library and include dire
-find_library(LIBSBML_STATIC_LIBRARY
-        NAMES libsbml-static.a libsbml-static.lib
-        PATHS ${LIBSBML_INSTALL_PREFIX}/lib
-        REQUIRED
-        )
-
-find_path(LIBSBML_INCLUDE_DIR
-        NAMES sbml/SBMLTypes.h
-        PATHS ${LIBSBML_INSTALL_PREFIX}/include
-        REQUIRED
-        )
 
 # build libcombine
 ExternalProject_Add(libCombine
@@ -125,14 +70,6 @@ ExternalProject_Add(libCombine
         -DEXTRA_LIBS=xml2|bz2|z|iconv #linux only, will need to change for windows
         )
 
-find_library(LIBCOMBINE_STATIC_LIB
-        NAMES libcombine-static.a
-        PATHS ${LIBCOMBINE_INSTALL_PREFIX}/lib
-        )
-find_path(LIBCOMBINE_INCLUDE_DIR
-        NAMES combine/combinearchive.h
-        PATHS ${LIBCOMBINE_INSTALL_PREFIX}/include
-        )
 
 ##################################################
 # get redland libraries for linking
@@ -169,17 +106,7 @@ find_path(LIBCOMBINE_INCLUDE_DIR
 #        )
 
 # raptor2
-find_library(RAPTOR2_STATIC_LIBRARY
-        NAMES libraptor2.a raptor2.a
-        PATHS ${RAPTOR2_INSTALL_PREFIX}/lib
-        /usr/local/lib
-        )
 
-find_path(RAPTOR2_INCLUDE_DIR
-        NAMES raptor2.h
-        PATHS ${RAPTOR2_INSTALL_PREFIX}/include/raptor2
-        /usr/local/include/raptor2
-        )
 
 
 # build rasqal
@@ -192,18 +119,6 @@ find_path(RAPTOR2_INCLUDE_DIR
 #        INSTALL_COMMAND "make install"
 #        )
 
-# rasqal
-find_library(RASQAL_STATIC_LIBRARY
-        NAMES librasqal.a rasqal.a
-        PATHS ${RASQAL_INSTALL_PREFIX}/lib
-        /usr/local/lib
-        )
-
-find_path(RASQAL_INCLUDE_DIR
-        NAMES rasqal.h
-        PATHS ${RASQAL_INSTALL_PREFIX}/include/rasqal
-        /usr/local/include/rasqal
-        )
 
 
 ## build librdf
@@ -216,32 +131,9 @@ find_path(RASQAL_INCLUDE_DIR
 #        INSTALL_COMMAND "make install"
 #        )
 # redland api
-find_library(REDLAND_STATIC_LIBRARY
-        NAMES librdf.a rdf.a
-        PATHS ${REDLAND_INSTALL_PREFIX}/lib
-        /usr/local/lib
 
-        )
-
-find_path(REDLAND_INCLUDE_DIR
-        NAMES librdf.h
-        PATHS ${REDLAND_INSTALL_PREFIX}/include
-        /usr/local/include
-        )
 
 ###############################################################
 # Call to build the original project
 #
 
-add_custom_target(raptor)
-add_custom_target(rasqal)
-add_custom_target(librdf)
-
-
-# FIXME add to default target "all"?
-ExternalProject_Add(libsemsim
-        DEPENDS zlib libsbml-dependencies zipper libsbml libCombine raptor rasqal librdf
-        SOURCE_DIR ${PROJECT_SOURCE_DIR}
-        CMAKE_ARGS -DHAVE_DEPENDENCIES=TRUE
-        BINARY_DIR ${CMAKE_BINARY_DIR}
-        )
