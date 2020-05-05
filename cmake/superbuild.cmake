a651f9e9a0f66d4c4f619675d5ef6e681267d80c
Ciaran Welsh <cwelsh2@uw.edu>
2020-05-05 16:19:56 +0100

    fix build

--- cmake/superbuild.cmake
+++ cmake/superbuild.cmake
@@ -23,12 +23,24 @@ ExternalProject_Add(libsbml-dependencies
         -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
         )
 
+# find zlib to give to zipper which depends on it
+find_library(ZLIB_STATIC_LIBRARY
+        NAMES libz.a z.a zlib.a libzlib.a zlib.dll libzlib.dll
+        PATHS ${ZLIB_INSTALL_PREFIX}/lib
+        REQUIRED
+        )
+
+find_path(ZLIB_INCLUDE_DIR
+        NAMES zlib.h
+        PATHS ${ZLIB_INSTALL_PREFIX}/include
+        REQUIRED
+        )
 
 # build zipper
 ExternalProject_Add(zipper
         SOURCE_DIR ${ZIPPER_SOURCE_DIR}
         BINARY_DIR ${ZIPPER_BINARY_DIR}
-        #        DEPENDS zlib
+        DEPENDS zlib
         CMAKE_ARGS
         -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
         -DCMAKE_INSTALL_PREFIX=${ZIPPER_INSTALL_PREFIX}
@@ -36,6 +48,42 @@ ExternalProject_Add(zipper
         -DZLIB_LIBRARY=${ZLIB_STATIC_LIBRARY}
         -DZLIB_INCLUDE_DIR=${ZLIB_INCLUDE_DIR}
         )
+
+
+# find zipper lib and include dirs
+find_library(ZIPPER_STATIC_LIBRARY
+        NAMES libZipper-static.a libZipper-static
+        PATHS ${ZIPPER_INSTALL_PREFIX}/lib
+        )
+find_path(ZIPPER_INCLUDE_DIR
+        NAMES zipper/zipper.h
+        PATHS ${ZIPPER_INSTALL_PREFIX}/include
+        )
+
+
+
+# find bz library and include dirs
+find_library(LIBBZ_STATIC_LIBRARY
+        NAMES libbz2.a
+        PATHS ${LIBSBML_DEPS_LIB_DIR}
+        )
+find_path(LIBBZ_INCLUDE_DIR
+        NAMES bzip2/bzlib.h
+        PATHS ${LIBSBML_DEPS_INCLUDE_DIR}
+        )
+
+# get libxml2 libraries
+find_library(LIBXML2_STATIC_LIBRARY
+        NAMES libxml2.a
+        )
+
+find_path(LIBXML2_INCLUDE_DIR
+        NAMES libxml/parser.h
+        PATHS /usr/local/include/libxml2
+        )
+
+
+
 
 # build libsbml
 ExternalProject_Add(libsbml
@@ -53,6 +101,19 @@ ExternalProject_Add(libsbml
         )
 
 
+# find the libsbml library and include dire
+find_library(LIBSBML_STATIC_LIBRARY
+        NAMES libsbml-static.a libsbml-static.lib
+        PATHS ${LIBSBML_INSTALL_PREFIX}/lib
+        REQUIRED
+        )
+
+find_path(LIBSBML_INCLUDE_DIR
+        NAMES sbml/SBMLTypes.h
+        PATHS ${LIBSBML_INSTALL_PREFIX}/include
+        REQUIRED
+        )
+
 # build libcombine
 ExternalProject_Add(libCombine
         SOURCE_DIR ${LIBCOMBINE_SOURCE_DIR}
@@ -68,6 +129,80 @@ ExternalProject_Add(libCombine
         -DZIPPER_INCLUDE_DIR=${ZIPPER_INCLUDE_DIR}
         -DZIPPER_LIBRARY=${ZIPPER_STATIC_LIBRARY}
         -DEXTRA_LIBS=xml2|bz2|z|iconv #linux only, will need to change for windows
+        )
+
+
+
+
+
+find_library(LIBCOMBINE_STATIC_LIB
+        NAMES libcombine-static.a
+        PATHS ${LIBCOMBINE_INSTALL_PREFIX}/lib
+        )
+find_path(LIBCOMBINE_INCLUDE_DIR
+        NAMES combine/combinearchive.h
+        PATHS ${LIBCOMBINE_INSTALL_PREFIX}/include
+        )
+
+
+find_library(RAPTOR2_STATIC_LIBRARY
+        NAMES libraptor2.a raptor2.a
+        PATHS ${RAPTOR2_INSTALL_PREFIX}/lib
+        /usr/local/lib
+        )
+
+find_path(RAPTOR2_INCLUDE_DIR
+        NAMES raptor2.h
+        PATHS ${RAPTOR2_INSTALL_PREFIX}/include/raptor2
+        /usr/local/include/raptor2
+        )
+
+# rasqal
+find_library(RASQAL_STATIC_LIBRARY
+        NAMES librasqal.a rasqal.a
+        PATHS ${RASQAL_INSTALL_PREFIX}/lib
+        /usr/local/lib
+        )
+
+find_path(RASQAL_INCLUDE_DIR
+        NAMES rasqal.h
+        PATHS ${RASQAL_INSTALL_PREFIX}/include/rasqal
+        /usr/local/include/rasqal
+        )
+
+find_library(REDLAND_STATIC_LIBRARY
+        NAMES librdf.a rdf.a
+        PATHS ${REDLAND_INSTALL_PREFIX}/lib
+        /usr/local/lib
+
+        )
+
+find_path(REDLAND_INCLUDE_DIR
+        NAMES librdf.h
+        PATHS ${REDLAND_INSTALL_PREFIX}/include
+        /usr/local/include
+        )
+
+
+SET(DEPENDENCIES
+        ${ZLIB_STATIC_LIBRARY}
+        ${ZLIB_INCLUDE_DIR}
+        ${ZIPPER_STATIC_LIBRARY}
+        ${ZIPPER_INCLUDE_DIR}
+        ${LIBBZ_STATIC_LIBRARY}
+        ${LIBBZ_INCLUDE_DIR}
+        ${LIBXML2_STATIC_LIBRARY}
+        ${LIBXML2_INCLUDE_DIR}
+        ${LIBSBML_STATIC_LIBRARY}
+        ${LIBSBML_INCLUDE_DIR}
+        ${LIBCOMBINE_STATIC_LIB}
+        ${LIBCOMBINE_INCLUDE_DIR}
+        ${RAPTOR2_STATIC_LIBRARY}
+        ${RAPTOR2_INCLUDE_DIR}
+        ${RASQAL_STATIC_LIBRARY}
+        ${RASQAL_INCLUDE_DIR}
+        ${REDLAND_STATIC_LIBRARY}
+        ${REDLAND_INCLUDE_DIR}
         )
 
 
