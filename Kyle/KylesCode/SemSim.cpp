2b62db0098d2f87a488787cdc77cc83e436df5a8
Ciaran Welsh <cwelsh2@uw.edu>
2020-05-06 22:58:31 +0100

    deleted Kyle's folder

--- Kyle/KylesCode/SemSim.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-#include "semsim/SemSim.h"
-
-void libsemgen_hello() {
-    std::cout << "Hello from libsemgen" << std::endl;
-}
--- Kyle/docs/examples/python/index.rst
+++
@@ -1,9 +0,0 @@
-.. _examples_python:
-
-Python
-======
-
-.. toctree::
-   :maxdepth: 1
-
-   create_sbml_model.rst
