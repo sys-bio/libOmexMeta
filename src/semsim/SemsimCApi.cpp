37d9fad6c7e2bbd68a41800bd94af749cb5e1a8f
Ciaran Welsh <cwelsh2@uw.edu>
2020-05-11 09:27:45 +0100

    Begin writing C and Python wrapper. Commit befoe refactoring tests

--- src/semsim/SemsimCApi.cpp
+++ src/semsim/SemsimCApi.cpp
@@ -3,5 +3,18 @@
 //
 
 #include "SemsimCApi.h"
+#include <iostream>
+
+semsim::RDF * rdf_new_ptr(){
+    return new semsim::RDF();
+}
+
+void RDF_fromString(semsim::RDF *rdf_ptr, const char *str, const char *format) {
+    semsim::RDF rdf = semsim::RDF::fromString(std::string(str), std::string(format));
+    rdf_ptr = &rdf;
+}
 
 
+//int AFunctionCalledX() {
+//    return 4;
+//}
