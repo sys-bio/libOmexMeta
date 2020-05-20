22710ca36630116a952c96aab848874682472323
Ciaran Welsh <cwelsh2@uw.edu>
2020-05-20 13:01:49 +0100

    Subject tests pass again

--- tests/cpp/SubjectTests.cpp
+++ tests/cpp/SubjectTests.cpp
@@ -17,9 +17,6 @@ public:
     }
 
     ~SubjectTests() override {
-//        librdf_free_storage(storage);
-//        librdf_free_model(model);
-//        librdf_free_world(world);
     }
 };
 
@@ -29,21 +26,21 @@ TEST_F(SubjectTests, TestDefaultConstructor) {
 }
 
 TEST_F(SubjectTests, TestSubjectFromUri) {
-    semsim::Subject subject(world_, "meta754");
+    semsim::Subject subject(world_, semsim::RDFURINode(world_.newNodeUriString("meta754")));
     std::string expected = subject.str();
     ASSERT_STREQ(expected.c_str(), "meta754");
 }
 
 TEST_F(SubjectTests, TestStringUri) {
     std::string url_str = "https://www.notarealaddress.com";
-    semsim::Subject subject(world_, url_str);;
+    semsim::Subject subject(world_, semsim::RDFURINode(world_.newNodeUriString(url_str)));
     const std::string &expected = subject.str();
     ASSERT_STREQ(url_str.c_str(), expected.c_str());
 }
 
 TEST_F(SubjectTests, TestGetNodeUri) {
     std::string url_str = "https://www.notarealaddress.com";
-    semsim::Subject subject(world_, url_str);;
+    semsim::Subject subject(world_, semsim::RDFURINode(world_.newNodeUriString(url_str)));
     semsim::LibrdfNode n = subject.getNode();
     std::string actual = n.str();
     ASSERT_STREQ(url_str.c_str(), actual.c_str());
@@ -51,7 +48,7 @@ TEST_F(SubjectTests, TestGetNodeUri) {
 
 TEST_F(SubjectTests, TestGetNodeBlank) {
     std::string string = "blank";
-    semsim::Subject subject(world_, string);;
+    semsim::Subject subject(world_, semsim::RDFURINode(world_.newNodeUriString(string)));
     semsim::LibrdfNode n = subject.getNode();
     std::string actual = n.str();
     ASSERT_STREQ(string.c_str(), actual.c_str());
