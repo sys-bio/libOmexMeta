a2267d613504da0a84fc5ccc0d74e30f9dc694d1
Ciaran Welsh <cwelsh2@uw.edu>
2020-05-22 16:03:25 +0100

    More tests. Fixed memory problem. Found some more

--- src/semsim/RedlandAPIWrappers/LibrdfStatement.cpp
+++ src/semsim/RedlandAPIWrappers/LibrdfStatement.cpp
@@ -7,8 +7,40 @@
 
 namespace semsim {
 
+    /*
+     * If you use librdf_free_statement(statement)
+     * you will be trying to free the statement
+     * AND the nodes associated with it. This
+     * conflicts the concept in Redland C++ API that
+     * each object is responsible for its own construction
+     * and destruction. The LibrdfNode class will free itself
+     * and therefore here we only free the statement.
+     */
+//    void free_statement(librdf_statement* statement){
+//        free(statement);
+//    }
+    void free_statement(librdf_statement *statement) {
+        /* dynamically or statically allocated? */
+        int is_dynamic;
+
+        if (!statement)
+            return;
+
+        is_dynamic = (statement->usage >= 0);
+
+        /* dynamically allocated and still in use? */
+        if (is_dynamic && --statement->usage)
+            return;
+
+        raptor_statement_clear(statement);
+
+        if (is_dynamic)
+            free(statement);
+    }
+
+
     LibrdfStatement::LibrdfStatement(librdf_statement *statement)
-            : statement_(statement_ptr(statement, librdf_free_statement)) {
+            : statement_(statement_ptr(statement, free_statement)) {
     }
 
     bool LibrdfStatement::operator==(const LibrdfStatement &rhs) const {
@@ -41,6 +73,20 @@ namespace semsim {
 
     librdf_statement *LibrdfStatement::get() {
         return statement_.get();
+    }
+
+    void LibrdfStatement::setSubject(LibrdfNode node) {
+        librdf_statement_set_subject(get(), node.get());
+    }
+
+    void LibrdfStatement::setPredicate(LibrdfNode node) {
+        librdf_statement_set_predicate(get(), node.get());
+
+    }
+
+    void LibrdfStatement::setResource(LibrdfNode node) {
+        librdf_statement_set_object(get(), node.get());
+
     }
 
 
--- tests/cpp/RedlandAPITests/LibrdfStatementTests.cpp
+++ tests/cpp/RedlandAPITests/LibrdfStatementTests.cpp
@@ -10,125 +10,56 @@
 class LibrdfStatementTests : public ::testing::Test {
 
 public:
+    semsim::LibrdfWorld world;
+    semsim::LibrdfNode subject = world.newNodeUriString("subject");
+    semsim::LibrdfNode predicate = world.newNodeUriString("predicate");
+    semsim::LibrdfNode resource = world.newNodeUriString("resource");
 
     LibrdfStatementTests() = default;
 
 };
 
 TEST_F(LibrdfStatementTests, TestCreate) {
-    semsim::LibrdfWorld world;
-    semsim::LibrdfNode subject = world.newNodeUriString("subject");
-    semsim::LibrdfNode predicate = world.newNodeUriString("predicate");
-    semsim::LibrdfNode resource = world.newNodeUriString("resource");
-
-    std::cout << subject.get() << std::endl;
-
-//    librdf_statement* stmt = librdf_new_statement_from_nodes(world.get(), subject.get(), predicate.get(), resource.get());
-////    librdf_node* node = librdf_statement_get_object(stmt);
-//
-//    librdf_free_statement(stmt);
-
-//    semsim::LibrdfStatement statement1 = world.newStatementFromNodes(subject, predicate, resource);
-
-
-//    semsim::LibrdfNode node = statement1.getSubjectNode();
-    int x = 4;
-
+    semsim::LibrdfStatement statement = world.newStatementFromNodes(subject, predicate, resource);
+    ASSERT_TRUE(true);
 }
-//TEST_F(LibrdfStatementTests, TestCopyConstructor) {
-//    semsim::LibrdfWorld world;
-//    semsim::LibrdfStatement statement1 = world.newStatementFromNodes(
-//            world.newNodeUriString("subject"),
-//            world.newNodeUriString("predicate"),
-//            world.newNodeUriString("resource")
-//    );
-//    semsim::LibrdfStatement statement2 = statement1;
-//    ASSERT_EQ(statement1, statement2);
-//}
 
-//TEST_F(LibrdfStatementTests, TestCopyAssignment) {
-//    semsim::LibrdfWorld world;
-//    semsim::LibrdfStorage storage1 = world.newStorage("memory", "semsim_store1");
-//    semsim::LibrdfStatement statement1 = world.newStatement(storage1);
-//    auto statement1_ptr_int = reinterpret_cast<std::uintptr_t>(*statement1.getStatement());
-//    semsim::LibrdfStatement statement2 = world.newStatement(storage1);
-//    auto statement2_ptr_int_before = reinterpret_cast<std::uintptr_t>(*statement2.getStatement());
-//    ASSERT_NE(statement1_ptr_int, statement2_ptr_int_before);
-//    statement2 = statement1;
-//    auto statement2_ptr_int_after = reinterpret_cast<std::uintptr_t>(*statement2.getStatement());
-//    ASSERT_EQ(statement1_ptr_int, statement2_ptr_int_after);
-//}
-//
-//
-//TEST_F(LibrdfStatementTests, TestMoveConstructor) {
-//    semsim::LibrdfWorld world;
-//    semsim::LibrdfStorage storage1 = world.newStorage("memory", "semsim_store1");
-//    semsim::LibrdfStatement statement1 = world.newStatement(storage1);
-//    auto statement1_int_ptr = reinterpret_cast<std::uintptr_t>(*statement1.getStatement());
-//    semsim::LibrdfStatement statement2 = std::move(statement1);
-//    auto statement2_int_ptr = reinterpret_cast<std::uintptr_t>(*statement2.getStatement());
-//    ASSERT_EQ(statement1_int_ptr, statement2_int_ptr);
-//}
-//
-//TEST_F(LibrdfStatementTests, TestMoveAssignment) {
-//    semsim::LibrdfWorld world;
-//    // storage to statement is 1:1
-//    semsim::LibrdfStorage storage1 = world.newStorage("memory", "semsim_store1");
-//    semsim::LibrdfStorage storage2 = world.newStorage("memory", "semsim_store2");
-//    semsim::LibrdfStatement statement1 = world.newStatement(storage1);
-//    auto statement1_int_ptr = reinterpret_cast<std::uintptr_t>(*statement1.getStatement());
-//    semsim::LibrdfStatement statement2 = world.newStatement(storage2);
-//    statement2 = std::move(statement1);
-//    auto statement2_int_ptr = reinterpret_cast<std::uintptr_t>(*statement2.getStatement());
-//    ASSERT_EQ(statement1_int_ptr, statement2_int_ptr);
-//}
+TEST_F(LibrdfStatementTests, TestCopyConstructor) {
+    semsim::LibrdfStatement statement1 = world.newStatementFromNodes(subject, predicate, resource);
+    semsim::LibrdfStatement statement2 = statement1;
+    ASSERT_EQ(statement1, statement2);
+}
+
+TEST_F(LibrdfStatementTests, TestMoveConstructor) {
+    semsim::LibrdfStatement statement1 = world.newStatementFromNodes(subject, predicate, resource);
+    semsim::LibrdfStatement statement2 = std::move(statement1);
+    std::string expected = "subject";
+    semsim::LibrdfNode node = statement2.getSubjectNode();
+    std::string actual = node.str();
+    ASSERT_STREQ(expected.c_str(), actual.c_str());
+}
+
+TEST_F(LibrdfStatementTests, TestCopyAssignment) {
+    semsim::LibrdfStatement statement1 = world.newStatementFromNodes(subject, predicate, resource);
+    semsim::LibrdfStatement statement2 = statement1;
+    std::string actual = statement2.getSubjectNode().str();
+    std::string expected = "subject";
+    ASSERT_STREQ(expected.c_str(), actual.c_str());
+}
+
+TEST_F(LibrdfStatementTests, TestMoveAssignment) {
+    semsim::LibrdfStatement statement1 = world.newStatementFromNodes(subject, predicate, resource);
+    semsim::LibrdfStatement statement2 = std::move(statement1);
+    std::string actual = statement2.getSubjectNode().str();
+    std::string expected = "subject";
+    ASSERT_STREQ(expected.c_str(), actual.c_str());
+}
 
 
-
-//TEST_F(LibrdfStatementTests, TestGetSubject) {
-//
-//    semsim::LibrdfWorld world;
-//    semsim::LibrdfNode subject = world.newNodeUriString("subject");
-//    semsim::LibrdfNode predicate = world.newNodeUriString("predicate");
-//    semsim::LibrdfNode resource = world.newNodeUriString("resource");
-//    semsim::LibrdfStatement statement1 = world.newStatementFromNodes(subject, predicate, resource);
-//    HERE();
-//    semsim::LibrdfNode n = statement1.getSubjectNode();
-//    std::string actual = n.str();
-////    std::string actual = statement1.getSubjectNode().str();
-//    std::string expected = "subject";
-//    HERE();
-//    ASSERT_EQ(expected.c_str(), actual.c_str());
-//    HERE();
-//}
-
-//TEST_F(LibrdfStatementTests, TestGetPredicate) {
-//    semsim::LibrdfWorld world;
-//    semsim::LibrdfNode subject = world.newNodeUriString("subject");
-//    semsim::LibrdfNode predicate = world.newNodeUriString("predicate");
-//    semsim::LibrdfNode resource = world.newNodeUriString("resource");
-//    semsim::LibrdfStatement statement1 = world.newStatementFromNodes(
-//            subject, predicate, resource
-//    );
-//    HERE();
-//    statement1.getPredicateNode().str();
-//    semsim::LibrdfNode predicate_out = statement1.getPredicateNode();
-//    HERE();
-//    predicate_out.str();
-//    HERE();
-
-
-//    ASSERT_EQ("predicate", statement1.getPredicateNode().str());
-//}
-
-//TEST_F(LibrdfStatementTests, TestGetObject) {
-//    semsim::LibrdfWorld world;
-//    semsim::LibrdfStatement statement1 = world.newStatementFromNodes(
-//            world.newNodeUriString("subject"),
-//            world.newNodeUriString("predicate"),
-//            world.newNodeUriString("resource")
-//    );
-//    ASSERT_EQ("resource", statement1.getObjectNode().str());
-//}
-
+TEST_F(LibrdfStatementTests, TestGetPredicateStr) {
+    semsim::LibrdfStatement statement1 = world.newStatementFromNodes(subject, predicate, resource);
+    std::string expected = "predicate";
+    std::string actual = statement1.getPredicateNode().str();
+    ASSERT_STREQ(expected.c_str(), actual.c_str());
+}
 
