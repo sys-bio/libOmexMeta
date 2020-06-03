4e88c26f9095e8085b0d02f79f91a799967e03fd
Ciaran Welsh <cwelsh2@uw.edu>
2020-06-03 17:37:07 +0100

    PhysicalForce tests pass without memory issues.

--- tests/cpp/PhysicalForceTests.cpp
+++ tests/cpp/PhysicalForceTests.cpp
@@ -29,82 +29,51 @@ public:
     };
 };
 
+
 TEST_F(PhysicalForceTests, TestPhysicalForceSubjectMetaidNode) {
-    PhysicalForce force(
-            model.get(),
-            Subject::fromRawPtr(LibrdfNode::fromUriString("MetaId004")),
-            physical_property,
-            std::vector<SourceParticipant>(
-                    {SourceParticipant(
-                            model.get(),
-                            "SourceId1",
-                            1.0,
-                            "PhysicalEntityReference1"
-                    )}
-            ),
-            std::vector<SinkParticipant>(
-                    {SinkParticipant(
-                            model.get(),
-                            "SinkId1",
-                            1.0,
-                            "PhysicalEntityReference2"
-                    )}
-            )
+    Subject subject_metaid = Subject::fromRawPtr(LibrdfNode::fromUriString("MetaId004"));
+    std::vector<SourceParticipant> source_participants(
+            {SourceParticipant(
+                    model.get(),
+                    "SourceId1",
+                    1.0,
+                    "PhysicalEntityReference1"
+            )}
+    );
+    std::vector<SinkParticipant> sink_participants(
+            {SinkParticipant(
+                    model.get(),
+                    "SinkId1",
+                    1.0,
+                    "PhysicalEntityReference2"
+            )}
+    );
+    std::vector<MediatorParticipant> mediator_participants(
+            {MediatorParticipant(
+                    model.get(),
+                    "MediatorID1",
+                    "PhysicalEntityReference3"
+            )}
     );
 
-    std::string actual = (const char *) librdf_uri_as_string(librdf_node_get_uri(force.getSubject().getNode()));
+    PhysicalForce force(
+            model.get(),
+            subject_metaid,
+            physical_property,
+            source_participants,
+            sink_participants
+    );
+    std::string actual = force.getSubject().str();
     std::string expected = "MetaId004";
     ASSERT_STREQ(expected.c_str(), actual.c_str());
-}
 
+    // Without Triple we need to free stuff manually
+    physical_property.free();
+    subject_metaid.free();
+    source_participants[0].free();
+    sink_participants[0].free();
+    mediator_participants[0].free();
 
-TEST_F(PhysicalForceTests, TestPhysicalForceSource) {
-    PhysicalForce force(
-            model.get(),
-            Subject::fromRawPtr(LibrdfNode::fromUriString("MetaId004")),
-            physical_property,
-            std::vector<SourceParticipant>(
-                    {SourceParticipant(
-                            model.get(),
-                            "SourceId1",
-                            1.0,
-                            "PhysicalEntityReference1"
-                    )}
-            ),
-            std::vector<SinkParticipant>(
-                    {SinkParticipant(
-                            model.get(),
-                            "SinkId1",
-                            1.0,
-                            "PhysicalEntityReference2"
-                    )}
-            )
-    );
-
-    Triples sources_triples = force.getSources()[0].toTriples("sources_metaid");
-    std::ostringstream actual;
-    for (auto &it: sources_triples) {
-        librdf_node_type node_type = librdf_node_get_type(it->getResource());
-
-        if (node_type == LIBRDF_NODE_TYPE_RESOURCE) {
-            actual <<
-                   librdf_uri_as_string(
-                           librdf_node_get_uri(
-                                   it->getResource()
-                           )
-                   ) <<
-                   std::endl;
-        } else if (node_type == LIBRDF_NODE_TYPE_LITERAL) {
-            actual <<
-                   librdf_node_get_literal_value(it->getResource()) <<
-                   std::endl;
-        }
-
-    }
-    std::string expected = "SourceId1\n"
-                           "PhysicalEntityReference1\n"
-                           "1\n";
-    ASSERT_STREQ(expected.c_str(), actual.str().c_str());
 }
 
 
@@ -209,25 +178,26 @@ TEST_F(PhysicalForceTests, TestPhysicalForceTriples) {
                            "    <semsim:hasSourceParticipant rdf:resource=\"SourceId1\"/>\n"
                            "  </rdf:Description>\n"
                            "  <rdf:Description rdf:about=\"SinkId1\">\n"
-                           "    <semsim:hasMultiplier rdf:datatype=\"http://www.w3.org/2001/XMLSchema#double\">1</semsim:hasMultiplier>\n"
+                           "    <semsim:hasMultiplier rdf:datatype=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#http://www.w3.org/2001/XMLSchema#double\">1</semsim:hasMultiplier>\n"
                            "    <semsim:hasPhysicalEntityReference rdf:resource=\"PhysicalEntityReference2\"/>\n"
                            "  </rdf:Description>\n"
                            "  <rdf:Description rdf:about=\"SourceId1\">\n"
-                           "    <semsim:hasMultiplier rdf:datatype=\"http://www.w3.org/2001/XMLSchema#double\">1</semsim:hasMultiplier>\n"
+                           "    <semsim:hasMultiplier rdf:datatype=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#http://www.w3.org/2001/XMLSchema#double\">1</semsim:hasMultiplier>\n"
                            "    <semsim:hasPhysicalEntityReference rdf:resource=\"PhysicalEntityReference1\"/>\n"
                            "  </rdf:Description>\n"
                            "</rdf:RDF>\n"
                            "";
-    std::cout << actual <<
-              std::endl;
+    std::cout << actual << std::endl;
     ASSERT_STREQ(expected.c_str(), actual.c_str());
 }
 
 
-TEST_F(PhysicalForceTests, TestPhysicalForceBuilder) {
-    PhysicalForce force(model.get());
+TEST(PhysicalForceTestsNoFixture, TestPhysicalForceBuilder) {
+    RDF rdf;
+    PhysicalForce force(rdf.getModel());
+    //todo considering implementing the builder as a composite builder
     force.setAbout("Force5")
-            .setPhysicalProperty(physical_property)
+            .setPhysicalProperty("OPB:OPB_00340")
             .addSource("Source1", 1, "PhysicalEntityReference1")
             .addSink("Sink1", 2, "PhysicalEntityReference2")
             .addSink("Sink2", 1, "PhysicalEntityReference3");
@@ -250,19 +220,18 @@ TEST_F(PhysicalForceTests, TestPhysicalForceBuilder) {
                            "    <semsim:hasSourceParticipant rdf:resource=\"Source1\"/>\n"
                            "  </rdf:Description>\n"
                            "  <rdf:Description rdf:about=\"Sink1\">\n"
-                           "    <semsim:hasMultiplier rdf:datatype=\"http://www.w3.org/2001/XMLSchema#double\">2</semsim:hasMultiplier>\n"
+                           "    <semsim:hasMultiplier rdf:datatype=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#http://www.w3.org/2001/XMLSchema#double\">2</semsim:hasMultiplier>\n"
                            "    <semsim:hasPhysicalEntityReference rdf:resource=\"PhysicalEntityReference2\"/>\n"
                            "  </rdf:Description>\n"
                            "  <rdf:Description rdf:about=\"Sink2\">\n"
-                           "    <semsim:hasMultiplier rdf:datatype=\"http://www.w3.org/2001/XMLSchema#double\">1</semsim:hasMultiplier>\n"
+                           "    <semsim:hasMultiplier rdf:datatype=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#http://www.w3.org/2001/XMLSchema#double\">1</semsim:hasMultiplier>\n"
                            "    <semsim:hasPhysicalEntityReference rdf:resource=\"PhysicalEntityReference3\"/>\n"
                            "  </rdf:Description>\n"
                            "  <rdf:Description rdf:about=\"Source1\">\n"
-                           "    <semsim:hasMultiplier rdf:datatype=\"http://www.w3.org/2001/XMLSchema#double\">1</semsim:hasMultiplier>\n"
+                           "    <semsim:hasMultiplier rdf:datatype=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#http://www.w3.org/2001/XMLSchema#double\">1</semsim:hasMultiplier>\n"
                            "    <semsim:hasPhysicalEntityReference rdf:resource=\"PhysicalEntityReference1\"/>\n"
                            "  </rdf:Description>\n"
-                           "</rdf:RDF>\n"
-                           "";
+                           "</rdf:RDF>\n";
     std::cout << actual << std::endl;
     ASSERT_STREQ(expected.c_str(), actual.c_str());
 }
