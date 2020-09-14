cea98686bc9bafc494b28699b6cc92554dd1c916
Ciaran Welsh <cwelsh2@uw.edu>
2020-09-14 12:41:17 +0100

    added johns test. Empty for now

---
+++ tests/cpp/JohnsTest.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Ciaran on 14/09/2020.
+//
+
+#include "gtest/gtest.h"
+#include "omexmeta/OmexMeta.h"
+#include "SBMLFactory.h"
+
+using namespace omexmeta;
+
+
+class JohnsTest : public ::testing::Test {
+public:
+    JohnsTest() = default;
+
+};
