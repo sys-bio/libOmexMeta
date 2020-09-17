ea8bfe9f019ad5ebd027958dd9504184fba24678
Ciaran Welsh <cwelsh2@uw.edu>
2020-09-17 11:42:07 +0100

    rebuild docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Ciaran Welsh'
 author = 'Ciaran Welsh'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '1.1.13'
 
 
 # -- General configuration ---------------------------------------------------
