82231cf9f6d2f94be79e4fb0b52de17bf64ff100
Ciaran Welsh <cwelsh2@uw.edu>
2021-06-04 17:23:15 +0100

    docs

--- docs/doxygen-output/html/search/functions_12.js
+++ docs/doxygen-output/html/search/functions_12.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['variablemetaid_585',['variableMetaId',['../classomexmeta_1_1PhysicalEntity.html#a6aff4d003c2e39aad8bd00ab536ec00e',1,'omexmeta::PhysicalEntity']]],
-  ['vcardtranslator_586',['VCardTranslator',['../classomexmeta_1_1VCardTranslator.html#a59320f47775f8b342d549ed92231c021',1,'omexmeta::VCardTranslator']]],
-  ['verify_587',['verify',['../classomexmeta_1_1Predicate.html#a1e7e59b8a48c9f89eeec73f3bbaea19c',1,'omexmeta::Predicate']]]
+  ['variablemetaid_590',['variableMetaId',['../classomexmeta_1_1PhysicalEntity.html#a6aff4d003c2e39aad8bd00ab536ec00e',1,'omexmeta::PhysicalEntity']]],
+  ['vcardtranslator_591',['VCardTranslator',['../classomexmeta_1_1VCardTranslator.html#a59320f47775f8b342d549ed92231c021',1,'omexmeta::VCardTranslator']]],
+  ['verify_592',['verify',['../classomexmeta_1_1Predicate.html#a1e7e59b8a48c9f89eeec73f3bbaea19c',1,'omexmeta::Predicate']]]
 ];
--- docs/doxygen-output/html/search/functions_b.js
+++ docs/doxygen-output/html/search/functions_b.js
@@ -1,12 +1,12 @@
 var searchData=
 [
-  ['namespaceknown_510',['namespaceKnown',['../classomexmeta_1_1Predicate.html#a8381c8b0c7bbaa27de29608cbff08bf5',1,'omexmeta::Predicate']]],
-  ['namespacemap_511',['namespaceMap',['../classomexmeta_1_1Predicate.html#a1291e3cd9727871f568e864e0f5af3f0',1,'omexmeta::Predicate']]],
-  ['newenergydiff_512',['newEnergyDiff',['../classomexmeta_1_1Editor.html#a4faf7d0c71afca8ded828ac7bdb857f5',1,'omexmeta::Editor']]],
-  ['newpersonalinformation_513',['newPersonalInformation',['../classomexmeta_1_1Editor.html#a1943079ddbc4a4c6d896f51f360a11df',1,'omexmeta::Editor']]],
-  ['newphysicalentity_514',['newPhysicalEntity',['../classomexmeta_1_1Editor.html#a245b2105c175892d1fddaf693fa9d636',1,'omexmeta::Editor']]],
-  ['newphysicalprocess_515',['newPhysicalProcess',['../classomexmeta_1_1Editor.html#a2815d918736ee17d07306c5cf07c8ebf',1,'omexmeta::Editor']]],
-  ['newphysicalproperty_516',['newPhysicalProperty',['../classomexmeta_1_1Editor.html#aeae47fb60d955f506de2f16a54c0679a',1,'omexmeta::Editor']]],
-  ['newphysicalpropertyptr_517',['newPhysicalPropertyPtr',['../classomexmeta_1_1Editor.html#ab36fc15581a0aa0af434af60d4f910ac',1,'omexmeta::Editor']]],
-  ['newsingularannotation_518',['newSingularAnnotation',['../classomexmeta_1_1Editor.html#a50674b2591d2fed572954ac8490ee21c',1,'omexmeta::Editor::newSingularAnnotation(std::string metaid) const'],['../classomexmeta_1_1Editor.html#a6142da2b89068e9638410c3c903e3b64',1,'omexmeta::Editor::newSingularAnnotation() const']]]
+  ['namespaceknown_514',['namespaceKnown',['../classomexmeta_1_1Predicate.html#a8381c8b0c7bbaa27de29608cbff08bf5',1,'omexmeta::Predicate']]],
+  ['namespacemap_515',['namespaceMap',['../classomexmeta_1_1Predicate.html#a1291e3cd9727871f568e864e0f5af3f0',1,'omexmeta::Predicate']]],
+  ['newenergydiff_516',['newEnergyDiff',['../classomexmeta_1_1Editor.html#a4faf7d0c71afca8ded828ac7bdb857f5',1,'omexmeta::Editor']]],
+  ['newpersonalinformation_517',['newPersonalInformation',['../classomexmeta_1_1Editor.html#a1943079ddbc4a4c6d896f51f360a11df',1,'omexmeta::Editor']]],
+  ['newphysicalentity_518',['newPhysicalEntity',['../classomexmeta_1_1Editor.html#a245b2105c175892d1fddaf693fa9d636',1,'omexmeta::Editor']]],
+  ['newphysicalprocess_519',['newPhysicalProcess',['../classomexmeta_1_1Editor.html#a2815d918736ee17d07306c5cf07c8ebf',1,'omexmeta::Editor']]],
+  ['newphysicalproperty_520',['newPhysicalProperty',['../classomexmeta_1_1Editor.html#aeae47fb60d955f506de2f16a54c0679a',1,'omexmeta::Editor']]],
+  ['newphysicalpropertyptr_521',['newPhysicalPropertyPtr',['../classomexmeta_1_1Editor.html#ab36fc15581a0aa0af434af60d4f910ac',1,'omexmeta::Editor']]],
+  ['newsingularannotation_522',['newSingularAnnotation',['../classomexmeta_1_1Editor.html#a50674b2591d2fed572954ac8490ee21c',1,'omexmeta::Editor::newSingularAnnotation(std::string metaid) const'],['../classomexmeta_1_1Editor.html#a6142da2b89068e9638410c3c903e3b64',1,'omexmeta::Editor::newSingularAnnotation() const']]]
 ];
